1e08eb2830796be0955e74e6e8d6df23524c1de3
mennahup <60627571+mennahup@users.noreply.github.com>
2020-10-23 15:04:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+/*this is a general main function for queue ,stack with both array and linkedlist implementation ,however ,it is here implemented commands for stack list
+if we want to try stack (array based implementation )we change only the included file 
+we only should include the queue file we want and use the wanted methods call
+*/
 //#pragma once
 #include<iostream>
 #include"stacklist.cpp"
+//#include"queuelist.cpp"
+//#include"queuearr.cpp"
+//#include"stackarr.cpp"
 void print_single(stack_input x){
    
     cout<<"value of user func"<<x<<endl;
@@ -21,7 +28,8 @@ int main(){
        cout<<"i"<<i<<endl;
         push(i,&s);
     }
-  /*  clear_stack(&s);
+   reverse_print(&s,&print_single);
+    clear_stack(&s);
     r=empty(&s);
     if(!r){
         cout<<"stack is empty"<<endl;
@@ -36,16 +44,6 @@ int main(){
         }
      cout<<"stack values"<<x<<endl;
     }
-    */
- reverse_print(&s,&print_single);
-
-/*push(2,&s);
-stack_input x1;
-pop(&x1,&s);
-cout<<"x"<<x1<<endl;
-stack_input y1;
-pop(&y1,&s);
-cout<<"y"<<y1<<endl;
-*/
+    
 
 }
